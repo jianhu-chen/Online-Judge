804c21067b8c4d609c13c98cf6b4dd01107a9e51
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-06-25 18:27:38 +0800

    Update 15.反转链表.py

--- Offer-Python/15.反转链表.py
+++ Offer-Python/15.反转链表.py
@@ -1,3 +1,22 @@
 ## 题目描述
 # 输入一个链表，反转链表后，输出新链表的表头。
 
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    # 返回ListNode
+    def ReverseList(self, pHead):
+        # write code here
+        if not pHead or not pHead.next:
+            return pHead
+        last = None
+        while pHead:
+            temp = pHead.next
+            pHead.next = last
+            # last 和 pHead 向前移动
+            last = pHead
+            pHead = temp
+        return last
