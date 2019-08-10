88ae1243f30f4e5b617c53f6d279e619e2d09b7c
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-08-11 01:24:09 +0800

    Create 42.和为S的两个数字.py

---
+++ Offer-Python/42.和为S的两个数字.py
@@ -0,0 +1,30 @@
+## 题目描述
+# 输入一个递增排序的数组和一个数字S，在数组中查找两个数，
+# 使得他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的。
+
+# 输出描述:
+# > 对应每个测试案例，输出两个数，小的先输出。
+
+
+# -*- coding:utf-8 -*-
+class Solution:
+    def FindNumbersWithSum(self, array, tsum):
+        # write code here
+        if not array:
+            return []
+        for i, a in enumerate(array):
+            try:
+                j = array.index(tsum - a)
+            except:
+                pass
+            else:
+                b = array[j]
+                return (a, b)
+        return []
+
+
+if __name__ == "__main__":
+    array,tsum = [1,2,4,7,11,15],15
+    s = Solution()
+    result = s.FindNumbersWithSum(array, tsum)
+    print(result)
