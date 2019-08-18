1381ac8bb634abf7677dd86aab60711239ff0de5
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-08-18 21:04:51 +0800

    Update 35.数组中的逆序对.py

--- Offer-Python/35.数组中的逆序对.py
+++ Offer-Python/35.数组中的逆序对.py
@@ -12,15 +12,52 @@
 # >
 # >	对于%100的数据,size<=2*10^5
 
+# -*- coding:utf-8 -*-
 class Solution:
     def InversePairs(self, data):
         # write code here
+        # 分治思想：左边数组中的逆序对的数量 + 右边数组中逆序对的数量 + 左右结合成新的顺序数组时
+        # 中出现的逆序对的数量
         if not data:
             return 0
-        count=0
-        for i in range(len(data)):
-            for j in range(0,i):
-                if data[j]>data[i]:
-                    count += 1
-        return count%1000000007
-    def merge(self, l1, l2):
+        
+
+# # -*- coding:utf-8 -*-
+# class Solution:
+#     def InversePairs(self, data):
+#         # write code here
+#         if not data:
+#             return 0
+#         temp = [i for i in data]
+#         return self.mergeSort(temp, data, 0, len(data)-1) % 1000000007
+       
+#     def mergeSort(self, temp, data, low, high):
+#         if low >= high:
+#             temp[low] = data[low]
+#             return 0
+#         mid = (low + high) / 2
+#         left = self.mergeSort(data, temp, low, mid)
+#         right = self.mergeSort(data, temp, mid+1, high)
+           
+#         count = 0
+#         i = low
+#         j = mid+1
+#         index = low
+#         while i <= mid and j <= high:
+#             if data[i] <= data[j]:
+#                 temp[index] = data[i]
+#                 i += 1
+#             else:
+#                 temp[index] = data[j]
+#                 count += mid-i+1
+#                 j += 1
+#             index += 1
+#         while i <= mid:
+#             temp[index] = data[i]
+#             i += 1
+#             index += 1
+#         while j <= high:
+#             temp[index] = data[j]
+#             j += 1
+#             index += 1
+#         return count + left + right
