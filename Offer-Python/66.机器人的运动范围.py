ffb8645571994a37f2cd1b06d640e9acdcb537fe
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-08-18 21:04:59 +0800

    Update 66.机器人的运动范围.py

--- Offer-Python/66.机器人的运动范围.py
+++ Offer-Python/66.机器人的运动范围.py
@@ -10,5 +10,33 @@
 class Solution:
     def movingCount(self, threshold, rows, cols):
         # write code here
-        pass
-        
+        if threshold <= 0:
+            return 0
+        visited = [0 for _ in range(rows * cols)]
+        self.moving(0, 0, rows, cols, threshold, visited)
+        return sum(visited)
+
+    def moving(self, i, j, rows, cols, threshold, visited):
+        if self.getSum(i) + self.getSum(j) > threshold:
+            return
+        if i >= 0 and j >= 0 and i < rows and j < cols and visited[i * cols + j] == 0:
+            visited[i * cols + j] = 1
+            self.moving(i-1, j, rows, cols, threshold, visited)
+            self.moving(i+1, j, rows, cols, threshold, visited)
+            self.moving(i, j-1, rows, cols, threshold, visited)
+            self.moving(i, j+1, rows, cols, threshold, visited)
+
+    def getSum(self, n):
+        if n < 0:
+            return 0
+        sum = 0
+        while n != 0:
+            unit = n % 10
+            sum += unit
+            n //= 10
+        return sum
+
+if __name__ == "__main__":
+    s =Solution()
+    result =  s.movingCount(5, 20, 20)
+    print(result)
