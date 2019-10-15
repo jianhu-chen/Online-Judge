d9787af8f2ce965ca634511c1603fa95ea24a322
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-10-15 20:53:39 +0800

    Update 16.合并两个排序的链表.py

--- Offer-Python/16.合并两个排序的链表.py
+++ Offer-Python/16.合并两个排序的链表.py
@@ -1,5 +1,6 @@
 ## 题目描述
-# 输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+# 输入两个单调递增的链表，输出两个链表合成后的链表，
+# 当然我们需要合成后的链表满足单调不减规则。
 
 # -*- coding:utf-8 -*-
 # class ListNode:
@@ -18,12 +19,15 @@ class Solution:
             else:
                 l.append(pHead2)
                 pHead2 = pHead2.next
+
         while pHead1:
             l.append(pHead1)
             pHead1 = pHead1.next
+
         while pHead2:
             l.append(pHead2)
             pHead2 = pHead2.next
+            
         if l != []:
             head = l[0]
             for i in range(len(l)-1):
