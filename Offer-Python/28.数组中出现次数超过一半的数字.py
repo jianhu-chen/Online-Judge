f1adcfa7c3dcff8ace01c6677281d86cffdd59b8
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-10-16 22:52:43 +0800

    Create 28.数组中出现次数超过一半的数字.py

---
+++ Offer-Python/28.数组中出现次数超过一半的数字.py
@@ -0,0 +1,17 @@
+## 题目描述
+# 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+# 例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
+# 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。
+# 如果不存在则输出0。
+
+
+# -*- coding:utf-8 -*-
+class Solution:
+    def MoreThanHalfNum_Solution(self, numbers):
+        # write code here
+        if not numbers:
+            return 0
+        for i in set(numbers):
+            if numbers.count(i)>len(numbers)/2:
+                return i
+        return 0
