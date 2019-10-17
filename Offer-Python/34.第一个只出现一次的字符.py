c0ffea978d0a86295aeb415cfa95ea420de2d263
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-10-17 11:39:55 +0800

    Create 34.第一个只出现一次的字符.py

---
+++ Offer-Python/34.第一个只出现一次的字符.py
@@ -0,0 +1,15 @@
+## 题目描述
+# 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,
+# 并返回它的位置, 如果没有则返回 -1（需要区分大小写）.
+
+
+# -*- coding:utf-8 -*-
+class Solution:
+    def FirstNotRepeatingChar(self, s):
+        # write code here
+        if not s:
+            return -1
+        ls = list(s)
+        for i in ls:
+            if ls.count(i)==1:
+                return ls.index(i)
