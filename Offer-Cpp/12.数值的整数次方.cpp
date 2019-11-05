39445ed3baa61118053361c06bb76d4e0ee99809
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-11-06 00:43:38 +0800

    Update 12.数值的整数次方.cpp

--- Offer-Cpp/12.数值的整数次方.cpp
+++ Offer-Cpp/12.数值的整数次方.cpp
@@ -7,15 +7,16 @@
 //
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 class Solution {
 public:
     double Power(double base, int exponent) {
-        if (base==0){
+        if (base == 0) {
             return 0;
         }
-        if(exponent==0){
+        if (exponent == 0) {
             return 1;
         }
         return pow(base, exponent);
