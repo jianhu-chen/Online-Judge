a8f3fc2247b305004a101c42eae32b2e7f70e7fa
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-11-14 15:55:33 +0800

    Create 1009.说反话.cpp

---
+++ PAT(BasicLevel)-Cpp/1009.说反话.cpp
@@ -0,0 +1,45 @@
+//
+// Created by jhchen<jianhuchen@163.com> on 2019-11-12 15:03:33.
+//
+// 题目链接: https://pintia.cn/problem-sets/994805260223102976/problems/994805314941992960
+// 题目描述: 给定一句英语，要求你编写程序，将句中所有单词的顺序颠倒输出。
+// 
+// ### 输入格式：
+// 
+// 测试输入包含一个测试用例，在一行内给出总长度不超过 80 的字符串。字符串由若干单词和若干空格组成，其中单词是由英文字母（大小写有区分）组成的字符串，单词之间用 1 个空格分开，输入保证句子末尾没有多余的空格。
+// 
+// ### 输出格式：
+// 
+// 每个测试用例的输出占一行，输出倒序后的句子。
+// 
+// ### 输入样例：
+// ```in
+// Hello World Here I Come
+// ```
+// 
+// ### 输出样例：
+// ```out
+// Come I Here World Hello
+// ```
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    string str;
+    stack<string> sta;
+    while (cin >> str) {
+        sta.push(str);
+    }
+    cout << sta.top();
+    sta.pop();
+    while (!sta.empty()) {
+        cout << " " << sta.top();
+        sta.pop();
+    }
+    return 0;
+}
+            
