293ee2403ad66692d9f2635b7970defd1b69ce47
jianhuchen <cjh18@mail.ustc.edu.cn>
2019-11-16 23:08:32 +0800

    Create 27.remove-element.py

---
+++ LeetCode-Python/27.remove-element.py
@@ -0,0 +1,108 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   27.remove-element.py
+@Time    :   2019/11/16 22:53:56
+@Author  :   jhchen
+@E-mail  :   jianhuchen@163.com
+@License :   Copyright(C), USTC
+@Desc    :   None
+'''
+#
+# @lc app=leetcode id=27 lang=python
+#
+# [27] Remove Element
+#
+# https://leetcode.com/problems/remove-element/description/
+#
+# algorithms
+# Easy (45.98%)
+# Likes:    1009
+# Dislikes: 2159
+# Total Accepted:    490.2K
+# Total Submissions: 1.1M
+# Testcase Example:  '[3,2,2,3]\n3'
+#
+# Given an array nums and a value val, remove all instances of that value
+# in-place and return the new length.
+#
+# Do not allocate extra space for another array, you must do this by modifying
+# the input array in-place with O(1) extra memory.
+#
+# The order of elements can be changed. It doesn't matter what you leave beyond
+# the new length.
+#
+# Example 1:
+#
+#
+# Given nums = [3,2,2,3], val = 3,
+#
+# Your function should return length = 2, with the first two elements of nums
+# being 2.
+#
+# It doesn't matter what you leave beyond the returned length.
+#
+#
+# Example 2:
+#
+#
+# Given nums = [0,1,2,2,3,0,4,2], val = 2,
+#
+# Your function should return length = 5, with the first five elements of nums
+# containing 0, 1, 3, 0, and 4.
+#
+# Note that the order of those five elements can be arbitrary.
+#
+# It doesn't matter what values are set beyond the returned length.
+#
+# Clarification:
+#
+# Confused why the returned value is an integer but your answer is an array?
+#
+# Note that the input array is passed in by reference, which means modification
+# to the input array will be known to the caller as well.
+#
+# Internally you can think of this:
+#
+#
+# // nums is passed in by reference. (i.e., without making a copy)
+# int len = removeElement(nums, val);
+#
+# // any modification to nums in your function would be known by the caller.
+# // using the length returned by your function, it prints the first len
+# elements.
+# for (int i = 0; i < len; i++) {
+# print(nums[i]);
+# }
+#
+
+# @lc code=start
+
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        nums.sort()
+
+        try:
+            i = nums.index(val)
+            j = i
+            while nums[j] == val:
+                j += 1
+                if j == len(nums):
+                    break
+            del nums[i:j]
+        except:
+            pass
+
+        return len(nums)
+
+
+# @lc code=end
+
+print(Solution().removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
