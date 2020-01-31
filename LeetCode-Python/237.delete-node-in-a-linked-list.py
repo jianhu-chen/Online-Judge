94f0cc5e5e0bf77ae1a23bc89b213639a7715233
jianhuchen <cjh18@mail.ustc.edu.cn>
2020-02-01 00:00:22 +0800

    Create 237.delete-node-in-a-linked-list.py

---
+++ LeetCode-Python/237.delete-node-in-a-linked-list.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   237.delete-node-in-a-linked-list.py
+@Time    :   2020/01/31 18:50:12
+@Author  :   jhchen
+@E-mail  :   jianhuchen@163.com
+@License :   Copyright(C), USTC
+@Desc    :   None
+'''
+#
+# @lc app=leetcode id=237 lang=python
+#
+# [237] Delete Node in a Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        
+# @lc code=end
+
