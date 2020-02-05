fd5f72f03dce4741a3888f15078b320ff12f134c
jianhuchen <cjh18@mail.ustc.edu.cn>
2020-02-06 00:01:14 +0800

    Create 349.intersection-of-two-arrays.py

---
+++ LeetCode-Python/349.intersection-of-two-arrays.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   349.intersection-of-two-arrays.py
+@Time    :   2020/02/05 23:37:14
+@Author  :   jhchen
+@E-mail  :   jianhuchen@163.com
+@License :   Copyright(C), USTC
+@Desc    :   None
+'''
+#
+# @lc app=leetcode id=349 lang=python
+#
+# [349] Intersection of Two Arrays
+#
+
+# @lc code=start
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1).intersection(set(nums2)))
+        
+# @lc code=end
+
