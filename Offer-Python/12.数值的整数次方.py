6c0918c6f9482aa8ee615e12dd125218a4d034ea
jianhu <cjh18@mail.ustc.edu.cn>
2022-06-06 20:47:00 +0800

    🎉  Init

--- Offer-Python/12.数值的整数次方.py
+++
@@ -1,9 +0,0 @@
-## 题目描述
-# 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
-
-
-# -*- coding:utf-8 -*-
-class Solution:
-    def Power(self, base, exponent):
-        # write code here
-        return base**exponent
--- Offer-Python/48.不用加减乘除做加法.py
+++
@@ -1,9 +0,0 @@
-## 题目描述
-# 写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。
-
-
-# -*- coding:utf-8 -*-
-class Solution:
-    def Add(self, num1, num2):
-        # write code here
-        return sum([num1, num2])
