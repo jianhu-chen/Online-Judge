c4f12ff1e9c72aa4317c3577ba17ef57e23b21c3
jianhu <cjh18@mail.ustc.edu.cn>
2022-08-05 13:37:07 +0800

    🐔 Update disjoint set

--- basic/disjoint_set/disjoint_set.py
+++ basic/disjoint_set/disjoint_set.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import unittest
 from typing import Any, List
 
 
 class DisjointSet:
+    """Disjoint set with path compression and union by rank."""
 
     def __init__(self, nodes: List[Any]) -> None:
         self.parent = {n: n for n in nodes}
         self.rank = {n: 1 for n in nodes}
 
-    def _find(self, node: Any) -> Any:
+    def is_same_set(self, node1: Any, node2: Any) -> bool:
+        """Check if two nodes are in the same set."""
+        return self.find(node1) == self.find(node2)
+
+    def find(self, node: Any) -> Any:
+        """Find the head of the given node."""
+        if node not in self.parent:
+            raise ValueError("node not in disjoint set!")
+
         path = []
         while self.parent[node] != node:
             path.append(node)
@@ -19,12 +29,10 @@ class DisjointSet:
             self.parent[path.pop()] = node
         return node
 
-    def is_same(self, node1: Any, node2: Any) -> bool:
-        return self._find(node1) == self._find(node2)
-
     def union(self, node1: Any, node2: Any) -> None:
-        h1 = self._find(node1)
-        h2 = self._find(node2)
+        """Union two disjoint sets."""
+        h1 = self.find(node1)
+        h2 = self.find(node2)
         if h1 == h2:
             return
 
@@ -36,4 +44,30 @@ class DisjointSet:
         self.rank.pop(small)
 
     def size(self) -> int:
+        """Return the number of disjoint sets."""
         return len(self.rank)
+
+
+class Tester(unittest.TestCase):
+
+    def test_disjoint_set(self):
+        disjoint_set = DisjointSet([1, 2, 3, 4, 5, 6])
+        disjoint_set.union(1, 2)
+        disjoint_set.union(2, 3)
+        disjoint_set.union(4, 5)
+        self.assertEqual(disjoint_set.size(), 3)
+        self.assertTrue(disjoint_set.is_same_set(1, 3))
+        self.assertFalse(disjoint_set.is_same_set(1, 4))
+
+        disjoint_set.union(1, 5)
+        self.assertEqual(disjoint_set.size(), 2)
+        self.assertTrue(disjoint_set.is_same_set(1, 5))
+        self.assertFalse(disjoint_set.is_same_set(1, 6))
+
+        disjoint_set.union(1, 6)
+        self.assertEqual(disjoint_set.size(), 1)
+        self.assertTrue(disjoint_set.is_same_set(2, 6))
+
+
+if __name__ == "__main__":
+    unittest.main()
