64906ef950e3778749cd0178284e48e2f9a585d2
jianhu <cjh18@mail.ustc.edu.cn>
2022-12-14 18:31:30 +0800

    refactor(heap): add `Heap.from_list()` method and refine code

--- basic/sort/heap.py
+++ basic/sort/heap.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from typing import Any, List, Callable
+import unittest
+from typing import Any, List, Callable, Iterable
 
 
 class Heap:
@@ -8,49 +9,31 @@ class Heap:
 
     Parameters
     ----------
-    max_size : int
-        Maximum size of heap.
     comparator : Callable[[Any, Any], bool]
         Comparator function.
-
-    Attributes
-    ----------
-    heap : List[Any]
-        Heap.
-    heap_size : int
-        Heap size.
-    heap_type : str
-        Heap type.
     """
 
-    def __init__(self, max_size: int, comparator: Callable = None) -> None:
-        self.max_size = max_size
+    def __init__(self, comparator: Callable = None) -> None:
         self.comparator = comparator if comparator else lambda a, b: a > b
-        self.heap: List[Any] = [0] * max_size
+        self.heap: List[Any] = []
         self.heap_size: int = 0
 
-    def build_heap(self, values: List[Any]):
-        """Build heap from values."""
-        if len(values) > self.max_size:
-            raise RuntimeError("Heap size is too small!")
+    @classmethod
+    def from_list(cls, values: Iterable[Any], inplace: bool = False, comparator: Callable = None):
+        """Build heap from list."""
+        instance = cls(comparator)
 
-        # deep copy
-        for v in values:
-            self.heap[self.heap_size] = v
-            self.heap_size += 1
+        if inplace:
+            instance.heap = values
+        else:
+            instance.heap = [*values]
+        instance.heap_size = len(values)
 
         # use heapify to build heap is faster than push all values
-        for i in range(len(values) - 1, -1, -1):
-            self.heapify(idx=i)
+        for i in reversed(range(len(values) // 2)):
+            instance.heapify(idx=i)
 
-    def push(self, value: Any) -> None:
-        """Heap push."""
-        if self.full():
-            raise RuntimeError("Heap is full!")
-
-        self.heap[self.heap_size] = value
-        self.heap_insert(idx=self.heap_size)
-        self.heap_size += 1
+        return instance
 
     def heap_insert(self, idx) -> None:
         """Heap insert from idx."""
@@ -63,38 +46,45 @@ class Heap:
             # update index
             idx = _parent(idx)
 
+    def heapify(self, idx: int) -> None:
+        """Heapify from idx."""
+        # get left child
+        left = 2 * idx + 1
+        # if left child exists, maybe right child exists
+        while left < self.heap_size:
+            if left + 1 < self.heap_size and self.comparator(self.heap[left + 1], self.heap[left]):
+                largest_idx = left + 1
+            else:
+                largest_idx = left
+            # compare with the largest child
+            if self.comparator(self.heap[idx], self.heap[largest_idx]):
+                break
+            # swap with the largest child
+            self.heap[idx], self.heap[largest_idx] = self.heap[largest_idx], self.heap[idx]
+            # update index
+            idx = largest_idx
+            # get left child
+            left = 2 * idx + 1
+
+    def push(self, value: Any) -> None:
+        """Heap push."""
+        self.heap[self.heap_size] = value
+        self.heap_insert(idx=self.heap_size)
+        self.heap_size += 1
+
     def pop(self) -> Any:
         """Heap pop."""
         if self.empty():
             raise RuntimeError("Heap is empty!")
 
         ans = self.heap[0]
-        # swap with last element
+        # swap with the last element
         self.heap[0], self.heap[self.heap_size - 1] = self.heap[self.heap_size - 1], self.heap[0]
         # update heap size
         self.heap_size -= 1
         # heapify
         self.heapify(idx=0)
         return ans
-
-    def heapify(self, idx: int) -> None:
-        """Heapify from idx."""
-        # get left child
-        left = 2 * idx + 1
-        while left < self.heap_size:  # if left child exists, maybe right child exists
-            if left + 1 < self.heap_size and self.comparator(self.heap[left + 1], self.heap[left]):
-                largest_idx = left + 1
-            else:
-                largest_idx = left
-            # compare with largest child
-            if self.comparator(self.heap[idx], self.heap[largest_idx]):
-                break
-            # swap with largest child
-            self.heap[idx], self.heap[largest_idx] = self.heap[largest_idx], self.heap[idx]
-            # update index
-            idx = largest_idx
-            # get left child
-            left = 2 * idx + 1
 
     def top(self) -> Any:
         """Heap top."""
@@ -110,6 +100,21 @@ class Heap:
         """Heap empty."""
         return self.heap_size == 0
 
-    def full(self) -> bool:
-        """Heap full."""
-        return self.heap_size == self.max_size
+    def clear(self) -> bool:
+        """Clean up useless data."""
+        del self.heap[self.heap_size:]
+
+
+class TestHeap(unittest.TestCase):
+
+    def test_from_list(self):
+        heap = Heap.from_list([4, 1, 5, 3, 9], )
+        self.assertEqual(heap.size(), 5)
+        self.assertEqual(heap.heap, [9, 4, 5, 3, 1])
+        heap = Heap.from_list([4, 1, 5, 3, 9], comparator=lambda a, b: a < b)
+        self.assertEqual(heap.size(), 5)
+        self.assertEqual(heap.heap, [1, 3, 5, 4, 9])
+
+
+if __name__ == "__main__":
+    unittest.main()
