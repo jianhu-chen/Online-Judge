b5fab29f49d76f1a3f3def2e462b723525a9f38f
jianhu <cjh18@mail.ustc.edu.cn>
2022-12-25 17:24:24 +0800

    feat: new problem
    number_of_islands

--- basic/graph/number_of_islands.py
+++ basic/graph/number_of_islands.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import random
-import unittest
 from typing import List
 
 
 class Islands:
-    """
-    给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+    """给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
 
     岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
 
@@ -17,19 +14,22 @@ class Islands:
     """
 
     def solution1(self, grid: List[List[str]]) -> int:
-        pass
+        """遇1改为2."""
+        M, N = len(grid), len(grid[0])
 
-    def solution2(self, grid: List[List[str]]) -> int:
-        pass
+        def infect(x, y):
+            if x < 0 or x > M - 1 or y < 0 or y > N - 1 or grid[x][y] != "1":
+                return
+            grid[x][y] = "2"
+            infect(x - 1, y)
+            infect(x + 1, y)
+            infect(x, y - 1)
+            infect(x, y + 1)
 
-    def solution3(self, grid: List[List[str]]) -> int:
-        pass
-
-class Tester(unittest.TestCase):
-
-    def test_solution(self):
-        pass
-
-
-if __name__ == "__main__":
-    unittest.main()
+        result = 0
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j] == "1":
+                    result += 1
+                    infect(i, j)
+        return result
