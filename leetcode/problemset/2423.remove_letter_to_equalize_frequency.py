14ade55a11975def654ae607c41ab93af3a9dc7e
jianhu <cjh18@mail.ustc.edu.cn>
2023-05-03 10:29:26 +0800

    feat: daily exercise

--- leetcode/problemset/2423.remove_letter_to_equalize_frequency.py
+++ leetcode/problemset/2423.remove_letter_to_equalize_frequency.py
@@ -6,7 +6,7 @@ from collections import Counter
 
 class Solution:
     def equalFrequency(self, word: str) -> bool:
-        for i, ch in enumerate(word):
+        for i in range(len(word)):
             cnt = Counter(word[:i] + word[i+1:])
             if len(set(cnt.values())) == 1:
                 return True
